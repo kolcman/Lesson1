ee20bb3c2918077e0b26c45ce48ce68ea763085a
Kolcman <m.kolcman@yandex.ru>
2024-02-05 12:58:22 +0300

    added some variables

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world!");
+﻿// Console.WriteLine("Hello world!");
+
+
+int value = 100;
+double d = 3.4;
+char ch = '!';
+bool b = false;
+Console.WriteLine(value);
+Console.WriteLine(d);
+Console.WriteLine(ch);
+Console.WriteLine(b);
