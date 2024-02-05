2655519aacee052d68fad55987ecee340034b107
Kolcman <m.kolcman@yandex.ru>
2024-02-05 16:15:25 +0300

    Task4 - complete

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5, b = 4, c = 8, d = 10, f = 12;
+int max = 0;
+
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (f > max)
+{
+    max = f;
+}
+Console.WriteLine(max);
